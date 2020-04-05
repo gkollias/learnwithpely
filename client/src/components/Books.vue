038bf90eb6a70f78576ec4630562d13ee34ca89c
George Kollias <george.kollias@booking.com>
2020-04-05 17:54:08 +0300

    Second change after CI

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-sm-10">
-        <h1>Books test 2</h1>
+        <h1>Books test 3</h1>
         <hr><br><br>
         <alert :message=message v-if="showMessage"></alert>
         <button type="button" class="btn btn-success btn-sm" v-b-modal.book-modal>Add Book</button>
