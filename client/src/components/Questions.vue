5cb17291d25e96342dd653dcb3695896635dbad8
George Kollias <george.kollias@booking.com>
2020-05-03 12:44:09 +0300

    Changed question management title

--- client/src/components/Questions.vue
+++ client/src/components/Questions.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-sm-10">
-        <h1>Questions test hot reload13</h1>
+        <h1>Questions Management</h1>
         <hr><br><br>
         <alert :message=message v-if="showMessage"></alert>
         <button type="button" class="btn btn-success btn-sm" v-b-modal.question-modal>
