5f90da8b7cc58e98ff8bffdeed3b718b97ca6ec5
George Kollias <george.kollias@booking.com>
2020-10-20 22:36:14 +0300

    Upgrade BootstrapVue version

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import Vue from 'vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import App from './App.vue';
 import router from './router';
 
@@ -12,7 +13,6 @@ import { Auth0Plugin } from './auth';
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
-
 
 // Install the authentication plugin here
 Vue.use(Auth0Plugin, {
