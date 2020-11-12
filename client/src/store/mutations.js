4a8008ab9cbcf21972d6c524e1c9972be7507c98
George Kollias <george.kollias@booking.com>
2020-11-12 11:11:16 +0200

    Disable es-linter warning, upgrade vue-cli

--- client/src/store/mutations.js
+++ client/src/store/mutations.js
@@ -7,15 +7,19 @@ import {
 
 export default {
   [SET_USER](state, user) {
+    // eslint-disable-next-line no-param-reassign
     state.user = user;
   },
   [SET_QUESTIONS](state, questions) {
+    // eslint-disable-next-line no-param-reassign
     state.questions = questions;
   },
   [SET_NEXT_QUESTION](state, id) {
+    // eslint-disable-next-line no-param-reassign
     state.currentQuestionId = id;
   },
   [SHOW_QUESTIONS](state, show) {
+    // eslint-disable-next-line no-param-reassign
     state.showQuestions = show;
   },
 };
