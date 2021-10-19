7abb8a723eeb5eb56f58a2263f5580d1becdea62
George Kollias <george.kollias@booking.com>
2021-10-19 22:46:03 +0200

    Intro description changes

--- client/src/components/intros/introFive.vue
+++ client/src/components/intros/introFive.vue
@@ -9,17 +9,26 @@
       <b-row>
         <div class="col-md-6 intro-five-LeftSection pb-5">
           <h1 class=" font-weight-bold text-white text-42 mb-3 t-shadow">
-            Διάβασε, παίξε, κέρδισε!
+            Διάβασε και παίξε!
           </h1>
           <p class=" text-20 mb-4">
-            Σ´αυτό το site μπορεἰς να κάνεις εξάσκηση στην γραμματική, το συντακτικό, την ορθογραφία
-            αλλά και στα μαθηματικά μέσα απο διαδραστικές ερωτήσεις.
+            Σ´αυτή την online εκπαιδευτική πλατφόρμα μπορείς να κάνεις εξάσκηση
+            <a style="text-decoration: underline;">για κάθε κεφάλαιο του σχολικού βιβλίου</a>
+            στη γλώσσα και στα μαθηματικά!
           </p>
           <div class=" mb-5 lead">
             <ul class="list-inline">
               <li class=" mb-3 ">
                 <i class="eva eva-checkmark-outline mr-2 text-warning" />
-                Ο στόχος είναι να μαζέψεις όσους πιο πολλούς πόντους μπορείς για να γίνεις genius!
+                Διάλεξε την τάξη στην οποία βρίσκεσαι
+              </li>
+              <li class=" mb-3 ">
+                <i class="eva eva-checkmark-outline mr-2 text-warning" />
+                Επίλεξε το μάθημα που σε ενδιαφέρει
+              </li>
+              <li class=" mb-3 ">
+                <i class="eva eva-checkmark-outline mr-2 text-warning" />
+                Κλίκαρε την ενότητα στην οποία θες να εξασκηθείς
               </li>
 
               <li class="mb-3">
@@ -29,9 +38,9 @@
               </li>
             </ul>
           </div>
-          <p class=" text-20 mb-4">
+          <!-- <p class=" text-20 mb-4">
             Καλη επιτυχία!!✌️
-          </p>
+          </p> -->
         </div>
         <b-col
           md="6"
